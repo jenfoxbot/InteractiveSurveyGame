84dd8ffc68e005bef1de9d99e7d4bea015d8f850
jenfoxbot <fox.jennifer.r@gmail.com>
2016-04-14 01:43:09 -0700

    Update InteractiveSurveyGame.py
    updated license name

--- InteractiveSurveyGame.py
+++ InteractiveSurveyGame.py
@@ -1,10 +1,10 @@
 #Makey Makey Survey Game!
 #Written by Jennifer Fox <jenfoxbot@gmail.com>
 '''----------------------------------------------------------------------------
- * "THE BEER-WARE LICENSE" (Revision 42):
+ * "THE COFFEE-WARE LICENSE" (Revision 42):
  * <jenfoxbot@gmail.com> wrote this file.  As long as you retain this notice you
  * can do whatever you want with this stuff. If we meet some day, and you think
- * this stuff is worth it, you can buy me a beer in return.
+ * this stuff is worth it, you can buy me a coffee in return.
  * ----------------------------------------------------------------------------
  '''
 from __future__ import print_function
